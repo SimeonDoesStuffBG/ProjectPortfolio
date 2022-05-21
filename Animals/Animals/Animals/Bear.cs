935c10dfe754259721c08e57eb87847a9ba22661
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-05-21 23:04:24 +0300

    Rabiit and Bear

---
+++ Animals/Animals/Animals/Bear.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+    class Bear:Animal
+    {
+        public Bear():base(30,new Food[] {Food.cow, Food.deer, Food.honey, Food.eggs }) { }
+    }
+
---
+++ Animals/Animals/Animals/Rabbit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class Rabbit : Animal
+    {
+    public Rabbit():base(5,new Food[] {Food.carrot, Food.letuce, Food.grass }) { }
+    }
+
